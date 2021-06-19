1d91530988dd7539a3425f3d8c6ce95046c51e01
maruf85 <maruf8500@gmail.com>
2021-06-19 22:36:52 +0600

    .editorconfig added

--- src/certifications/certifications.module.ts
+++ src/certifications/certifications.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CertificationsService } from './certifications.service';
 import { CertificationsController } from './certifications.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProfileSchema } from 'src/profiles/schemas/profile.schema';
 import { CertificationSchema } from './schemas/certification.schema';
+import { CertificationsService } from './certifications.service';
 
 @Module({
   imports: [
